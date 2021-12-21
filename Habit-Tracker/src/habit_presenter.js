86b750bdec8d364fbc83b5be6bd0cd50db7cde8e
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-21 20:01:00 +0900

    [FE] update : 유닛 테스트 작성 학습 및 @types/jest

--- Habit-Tracker/src/habit_presenter.js
+++ Habit-Tracker/src/habit_presenter.js
@@ -1,6 +1,7 @@
 export class HabitPresenter {
-  constructor(habits) {
+  constructor(habits, maxHabits) {
     this.habits = habits;
+    this.maxHabits = maxHabits;
   }
 
   getHabits() {
@@ -21,7 +22,7 @@ export class HabitPresenter {
   decrement(habit, update) {
     this.habits = this.habits.map((item) => {
       if (item.id === habit.id) {
-        const count = habit.count - 1;
+        const count = item.count - 1;
         return { ...habit, count: count < 0 ? 0 : count };
       }
       return item;
@@ -35,6 +36,11 @@ export class HabitPresenter {
   }
 
   add(name, update) {
+    if (this.habits.length === this.maxHabits) {
+      throw new Error(
+        `습관의 갯수는 ${this.maxHabits}개 이상이 될 수 없습니다.`
+      );
+    }
     this.habits = [...this.habits, { id: Date.now(), name, count: 0 }];
     update(this.habits);
   }
---
+++ Habit-Tracker/src/tests/habit_presenter.test.js
@@ -0,0 +1,80 @@
+import { HabitPresenter } from "../habit_presenter";
+
+describe("HabitPresenter", () => {
+  let presenter;
+  let update;
+
+  const habits = [
+    { id: 1, name: "Reading", count: 1 },
+    { id: 2, name: "Running", count: 0 },
+  ];
+
+  beforeEach(() => {
+    presenter = new HabitPresenter(habits, 3);
+    update = jest.fn();
+  });
+
+  it("Inits with habits", () => {
+    expect(presenter.getHabits()).toEqual(habits);
+  });
+
+  it("Increments habit count and call update callback", () => {
+    presenter.increment(habits[0], update);
+
+    expect(presenter.getHabits()[0].count).toBe(2);
+    checkUpdateIsCalled();
+  });
+
+  it("Decrements habit count and call update callback", () => {
+    presenter.decrement(habits[0], update);
+
+    expect(presenter.getHabits()[0].count).toBe(0);
+    checkUpdateIsCalled();
+  });
+
+  it("Does not set the count value below 0 when decrements", () => {
+    presenter.decrement(habits[0], update);
+    presenter.decrement(habits[0], update);
+
+    expect(presenter.getHabits()[0].count).toBe(0);
+  });
+
+  it("Delete habit from the list and call update callback", () => {
+    presenter.delete(habits[0], update);
+
+    expect(presenter.getHabits().length).toBe(1);
+    expect(presenter.getHabits()[0].name).toBe("Running");
+    checkUpdateIsCalled();
+  });
+
+  it("Adds new habits to the list and call update callback", () => {
+    presenter.add("Eating", update);
+
+    expect(presenter.getHabits()[2].name).toBe("Eating");
+    expect(presenter.getHabits()[2].count).toBe(0);
+
+    checkUpdateIsCalled();
+  });
+
+  it("resets all habit count to 0", () => {
+    presenter.reset(update);
+
+    expect(presenter.getHabits()[0].count).toBe(0);
+    expect(presenter.getHabits()[1].count).toBe(0);
+
+    checkUpdateIsCalled();
+  });
+
+  it("Throws an error when the max habits limit is exceeded", () => {
+    presenter.add("Eating", update);
+
+    expect(() => {
+      presenter.add("Eating", update);
+    }).toThrow(`습관의 갯수는 3개 이상이 될 수 없습니다.`);
+  });
+
+  function checkUpdateIsCalled() {
+    expect(update).toHaveBeenCalledTimes(1);
+    expect(update).toHaveBeenCalledWith(presenter.getHabits());
+  }
+});
