efcdbb8040355811a3c81db5c615a121b5015a04
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-22 20:20:03 +0900

    [FE] Add : 스냅샷 테스트 학습 및 react-test-renderer 설치

--- Habit-Tracker/src/components/form/test/habitAddForm.test.js
+++ Habit-Tracker/src/components/form/test/habitAddForm.test.js
@@ -2,33 +2,43 @@ import React from "react";
 import { screen, render } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import HabitAddForm from "../habitAddForm";
+import renderer from "react-test-renderer";
 
 describe("HabitAddForm", () => {
-  let onAdd;
-  let input;
-  let button;
+  it("Renders", () => {
+    // 스냅샷 테스트를 진행
+    const component = renderer.create(<HabitAddForm onAdd={jest.fn()} />);
 
-  beforeEach(() => {
-    onAdd = jest.fn();
-    render(<HabitAddForm onAdd={onAdd} />);
-    input = screen.getByPlaceholderText("Habit");
-    button = screen.getByText("Add");
+    expect(component.toJSON()).toMatchSnapshot();
   });
 
-  it("Calls onAdd when button is clicked and valid habit is entered", () => {
-    // input에 다가 원하는 습관의 이름을 타이핑 한 다음에
-    // add라는 버튼을 클릭하면
-    // onAdd가 input에 입력된 이름과 함께 호출되어야 함!
-    userEvent.type(input, "New Habit");
-    userEvent.click(button);
+  describe("Form Submit", () => {
+    let onAdd;
+    let input;
+    let button;
 
-    expect(onAdd).toHaveBeenCalledWith("New Habit");
-  });
+    beforeEach(() => {
+      onAdd = jest.fn();
+      render(<HabitAddForm onAdd={onAdd} />);
+      input = screen.getByPlaceholderText("Habit");
+      button = screen.getByText("Add");
+    });
 
-  it("Does not call onAdd when the habit is empty", () => {
-    userEvent.type(input, "");
-    userEvent.click(button);
+    it("Calls onAdd when button is clicked and valid habit is entered", () => {
+      // input에 다가 원하는 습관의 이름을 타이핑 한 다음에
+      // add라는 버튼을 클릭하면
+      // onAdd가 input에 입력된 이름과 함께 호출되어야 함!
+      userEvent.type(input, "New Habit");
+      userEvent.click(button);
 
-    expect(onAdd).toHaveBeenCalledTimes(0);
+      expect(onAdd).toHaveBeenCalledWith("New Habit");
+    });
+
+    it("Does not call onAdd when the habit is empty", () => {
+      userEvent.type(input, "");
+      userEvent.click(button);
+
+      expect(onAdd).toHaveBeenCalledTimes(0);
+    });
   });
 });
