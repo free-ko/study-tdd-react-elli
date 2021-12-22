a14166d72ab5b2837c05c07cd023a049c0edb14e
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-22 20:07:02 +0900

    [FE] Add : React HabitAddForm 컴포넌트 테스트 학습

---
+++ Habit-Tracker/src/components/form/test/habitAddForm.test.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { screen, render } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import HabitAddForm from "../habitAddForm";
+
+describe("HabitAddForm", () => {
+  let onAdd;
+  let input;
+  let button;
+
+  beforeEach(() => {
+    onAdd = jest.fn();
+    render(<HabitAddForm onAdd={onAdd} />);
+    input = screen.getByPlaceholderText("Habit");
+    button = screen.getByText("Add");
+  });
+
+  it("Calls onAdd when button is clicked and valid habit is entered", () => {
+    // input에 다가 원하는 습관의 이름을 타이핑 한 다음에
+    // add라는 버튼을 클릭하면
+    // onAdd가 input에 입력된 이름과 함께 호출되어야 함!
+    userEvent.type(input, "New Habit");
+    userEvent.click(button);
+
+    expect(onAdd).toHaveBeenCalledWith("New Habit");
+  });
+
+  it("Does not call onAdd when the habit is empty", () => {
+    userEvent.type(input, "");
+    userEvent.click(button);
+
+    expect(onAdd).toHaveBeenCalledTimes(0);
+  });
+});
