a036c6add5674f0c37ce5dffee39f3c74fd59cf0
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-25 11:36:59 +0900

    [FE] update HabitPresenter Export 수정

--- Habit-Tracker/src/app/app.jsx
+++ Habit-Tracker/src/app/app.jsx
@@ -1,32 +1,44 @@
-import React from 'react';
-import { useCallback } from 'react';
-import { useState } from 'react';
-import './app.css';
-import Habits from '../components/habits/habits';
-import Navbar from '../components/navbar/navbar';
+import React from "react";
+import { useCallback } from "react";
+import { useState } from "react";
+import "./app.css";
+import Habits from "../components/habits/habits";
+import Navbar from "../components/navbar/navbar";
 
 const App = ({ presenter }) => {
   const [habits, setHabits] = useState(presenter.getHabits());
 
-  const handleIncrement = useCallback((habit) => {
-    presenter.increment(habit, setHabits);
-  }, []);
+  const handleIncrement = useCallback(
+    (habit) => {
+      presenter.increment(habit, setHabits);
+    },
+    [presenter]
+  );
 
-  const handleDecrement = useCallback((habit) => {
-    presenter.decrement(habit, setHabits);
-  }, []);
+  const handleDecrement = useCallback(
+    (habit) => {
+      presenter.decrement(habit, setHabits);
+    },
+    [presenter]
+  );
 
-  const handleDelete = useCallback((habit) => {
-    presenter.delete(habit, setHabits);
-  }, []);
+  const handleDelete = useCallback(
+    (habit) => {
+      presenter.delete(habit, setHabits);
+    },
+    [presenter]
+  );
 
-  const handleAdd = useCallback((name) => {
-    presenter.add(name, setHabits);
-  }, []);
+  const handleAdd = useCallback(
+    (name) => {
+      presenter.add(name, setHabits);
+    },
+    [presenter]
+  );
 
   const handleReset = useCallback(() => {
     presenter.reset(setHabits);
-  }, []);
+  }, [presenter]);
 
   return (
     <>
