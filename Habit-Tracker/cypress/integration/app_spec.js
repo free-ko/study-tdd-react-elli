0f69e87c2247911e798540ef4d19ea19fe42bf7a
kyw <47565280+free-ko@users.noreply.github.com>
2021-12-29 21:56:04 +0900

    [FE] Add : Cypress 앱 테스트 코드 학습

--- Habit-Tracker/cypress/integration/app_spec.js
+++ Habit-Tracker/cypress/integration/app_spec.js
@@ -1,6 +1,4 @@
 /// <reference types="cypress" />;
-import cy from "cypress";
-
 import "@testing-library/cypress/add-commands";
 
 describe("Habit Tracker", () => {
@@ -8,7 +6,50 @@ describe("Habit Tracker", () => {
     cy.visit("/");
   });
 
-  cy.it("Renders", () => {
+  it("Renders", () => {
     cy.findByText("Habit Tracker").should("exist");
+  });
+
+  it("Adds new habit at the end", () => {
+    cy.findByPlaceholderText("Habit").type("New Habit");
+    cy.findBytext("Add").click();
+    cy.findAllByTestId("habit-name").last().should("have.text", "New Habit");
+    cy.findAllByTestId("habit-count").last().should("have.text", "0");
+  });
+
+  it("Increase count", () => {
+    cy.findAllByTitle("increase").first().click();
+    cy.findAllByTestId("habit-count").first().should("have.text", "1");
+  });
+
+  it("Decrease count", () => {
+    cy.findAllByTitle("increase").first().click();
+    cy.findAllByTitle("decrease").first().click();
+    cy.findAllByTestId("habit-count").first().should("have.text", "0");
+  });
+
+  it("Does not decreases below 0", () => {
+    cy.findAllByTitle("decrease").first().click();
+    cy.findAllByTestId("habit-count").first().should("have.text", "0");
+  });
+
+  it("Shows active count on the header", () => {
+    cy.findAllByTitle("increase").first().click();
+    cy.findAllByTitle("increase").first().click();
+    cy.findByTestId("total-count").should("have.text", "2");
+  });
+
+  it("Reset to 0 when clicking reset all", () => {
+    cy.findAllByTitle("increase").first().click();
+    cy.findAllByTitle("increase").first().click();
+    cy.findByText("Reset All").click();
+    cy.findAllByTestId("habit-count").each((item) => {
+      cy.wrap(item).should("have.text", "0");
+    });
+  });
+
+  it("Deletes an items", () => {
+    cy.findAllByTitle("delete").first().click();
+    cy.findAllByTestId("habit-name").findByText("Reading").should("not.exist");
   });
 });
